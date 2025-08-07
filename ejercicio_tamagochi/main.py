5cde20ce8b1f0fd116e6d155834a3096ab75fee9
catalinamonserrat <kata1800@gmail.com>
2025-08-07 18:42:14 -0400

    Modificacion Main.py ejercicio Tamagotchi

--- ejercicio_tamagochi/main.py
+++ ejercicio_tamagochi/main.py
@@ -2,23 +2,74 @@ from tamagotchi import Tamagotchi
 from persona import Persona
 from tamagochi_dragon import Dragon
 
-#Crear tamagotchi
-Mascota1 = Tamagotchi("Pinky", "Rosa")
-#Crear persona
-p1 = Persona("Lucia", "Ramírez", Mascota1)
+# Inicializar variables globales
+p1 = None
+Mascota = None
+es_dragon = False
 
-p1.jugar_con_tamagotchi()
-print("\n")
-p1.darle_comida()
-print("\n")
-p1.curar_tamagotchi()
-print("\n")
+#Bienvenida
+print("=== Bienvenido a Mascota virtual ===")
+print("Por favor cree su usuario...")
 
-#creando tamagochi especial
-Mascota2 = Dragon("Tairn", "negro", "Lanzar fuego")
-#Crear persona
-p2 = Persona("Diego", "Garcia", Mascota2)
+nombre = input("Ingrese su nombre: ")
+apellido = input("Ingrese su apellido: ")
 
-p2.jugar_con_tamagotchi()
-print("\n")
-Mascota2.usar_superpoder()
+# Elección de mascota
+print("=== Elige tu mascota Tamagotchi ===")
+print("1. Tamagotchi normal")
+print("2. Tamagotchi Dragon")
+
+eleccion = int(input("Elige una opción: "))
+
+if eleccion == 1:
+    nombre_mascota = input("Ingrese el nombre de su Tamagotchi: ")
+    color_mascota = input("Ingrese el color de su Tamagotchi: ")
+    Mascota = Tamagotchi(nombre_mascota, color_mascota)
+    p1 = Persona(nombre, apellido, Mascota)
+    es_dragon = False
+
+elif eleccion == 2:
+    nombre_mascota = input("Ingrese el nombre de su Tamagotchi dragon: ")
+    color_mascota = input("Ingrese el color de su Tamagotchi Dragon: ")
+    superpoder = input("Ingrese el superpoder de su mascota: ")
+    Mascota = Dragon(nombre_mascota, color_mascota, superpoder)
+    p1 = Persona(nombre, apellido, Mascota)
+    es_dragon = True
+else:
+    print("Opcion no valida, se asignara un Tamagotchi por defecto")
+    Mascota = Tamagotchi("Pinky", "Rosa")
+    persona = Persona(nombre, apellido, Mascota)
+    es_dragon = False
+
+    #Menú Interactivo 
+def mostrar_menu():
+    print("\n=== Bienvenido a Mascota virtual ===")
+    print("1. Jugar con Tamagotchi")
+    print("2. Darle de comer")
+    print("3. Curarlo")
+    print("4. Ver estado")
+    if es_dragon:
+        print("5. Usar superpoder")
+        print("6. Salir")
+    else:
+        print("5. Salir")
+
+while True:
+    mostrar_menu()
+    opcion = input("Ingrese una opcion: ")
+
+    if opcion == "1":
+        p1.jugar_con_tamagotchi()
+    elif opcion == "2":
+        p1.darle_comida()
+    elif opcion == "3":
+        p1.curar_tamagotchi()
+    elif opcion == "4":
+        Mascota.mostrar_estatisticas()
+    elif opcion == "5" and es_dragon:
+        Mascota.usar_superpoder()
+    elif (opcion == "5" and not es_dragon) or (opcion == "6" and es_dragon): 
+        print("Gracias por jugar, saliendo del programa...")
+        break   
+    else:
+        print("Opcion no valida, intente nuevamente.")
--- ejercicio_tamagochi/tamagotchi.py
+++ ejercicio_tamagochi/tamagotchi.py
@@ -6,6 +6,13 @@ class Tamagotchi:
         self.felicidad = felicidad
         self.energia = energia
 
+    def mostrar_estatisticas(self):
+        print(f"Nombre: {self.nombre}")
+        print(f"Color: {self.color}")
+        print(f"Salud: {self.salud}")
+        print(f"Felicidad: {self.felicidad}")
+        print(f"Energia: {self.energia}")
+    
     def jugar(self):
         if self.salud < 5:
             print(f"El tamagotchi {self.nombre} está enfermo, su salud es {self.salud} y no puede jugar.")
@@ -13,15 +20,18 @@ class Tamagotchi:
         
         self.felicidad = min(self.felicidad + 10, 100) #aumenta la felicidad en 10
         self.salud = max(self.salud - 5, 0) #disminuye la salud en 5
-        print(f"El tamagotchi {self.nombre} está jugando. Su felicidad es {self.felicidad} y su salud es {self.salud}.")
+        self.energia = max(self.energia - 10, 0) #disminuye la energia en 10
 
+        print(f"El tamagotchi {self.nombre} está jugando. Valores actuales: felicidad {self.felicidad}, salud {self.salud} y energia {self.energia}.")
 
     def comer(self):
         self.salud = min(self.salud + 10, 100) #aumenta la salud en 10
         self.felicidad = min(self.felicidad + 5, 100) #aumenta la felicidad en 5
-        print(f"El tamagotchi {self.nombre} está comiendo y su salud es {self.salud} y su felicidad es {self.felicidad}.")
+        self.energia = min(self.energia + 5, 100) #Aumneta la energia en 5
+        print(f"El tamagotchi {self.nombre} está comiendo, Sus valores actualizados son: salud {self.salud}, energia {self.energia} y felicidad {self.felicidad}.")
 
     def curar(self):
         self.salud = min(self.salud + 20, 100) #aumenta la salud en 20
         self.felicidad = max(self.felicidad - 5, 0) #disminuye la felicidad en 5
-        print(f"El tamagotchi {self.nombre} está curandose y su salud es {self.salud} y su felicidad es {self.felicidad}.")
+        self.energia = max(self.energia - 15, 0) #disminuye la energia en 10
+        print(f"El tamagotchi {self.nombre} está curandose. Sus valores actuales son: salud {self.salud}, energia {self.energia} y felicidad {self.felicidad}.")
